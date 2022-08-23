5680122086ff5e30191a4150b94a8d48792c8e20
mikeg Gronka <michal.gronka@tomandco.co.uk>
2022-08-23 21:40:50 +0200

    Preparing test config

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,13 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  console.log(err);
+  return false;
+});
+
+describe("Test scenario 1", () => {
+  it("Opens SmartFrame 1", () => {
+    cy.visit("https://smartitnow.blogspot.com/p/e.html");
+    cy.get("iframe");
+  });
+});
