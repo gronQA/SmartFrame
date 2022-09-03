cdbbfe51376a23dd34e347c95c7541de23c49692
mikeg Gronka <michal.gronka@tomandco.co.uk>
2022-09-03 16:37:50 +0200

    No sleep removed from scenario1.spec, parallel flag added to package.json

--- test/scenario1.spec.js
+++ test/scenario1.spec.js
@@ -1,6 +1,5 @@
 const { Builder, By } = require("selenium-webdriver");
 const assert = require("assert");
-const { threadId } = require("worker_threads");
 const expectedText =
   "An image (from Latin: imago) is an artifact that depicts visual perception, such as a photograph or other two-dimensional picture, that resembles a subject—usually a physical object—and thus provides a depiction of it. In the context of signal processing, an image is a distributed amplitude of color(s). A pictorial script is a writing system that employs images as symbols for various semantic entities, rather than the abstract signs used by alphabets.";
 
@@ -27,29 +26,22 @@ describe("Test scenario 1", function () {
       .switchTo()
       .frame(driver.findElement(By.className("smartframe-embed")));
 
-    await driver.sleep(2000);
-
     // Check if the caption is correctly displayed
+    await driver.wait(() =>
+      driver.findElement(By.className("caption-wrapper")).getText()
+    );
     const actualText = await driver
       .findElement(By.className("caption-wrapper"))
-      .getText()
-      .then(function (value) {
-        return value;
-      });
-
+      .getText();
     assert.strictEqual(actualText, expectedText);
 
     // Click on the icon in the top-left-hand corner of the SmartFrame
     await driver.findElement(By.className("action-buttons__caption")).click();
 
-    await driver.sleep(2000);
-
     // Check if the layer opens
-    const isLayerOpen = await driver
-      .findElement(By.className("hide-copyright"))
-      .isDisplayed();
-
-    assert.strictEqual(isLayerOpen, true);
+    const layer = await driver.findElement(By.className("hide-copyright"));
+    await driver.wait(() => layer.isDisplayed());
+    assert.strictEqual(await layer.isDisplayed(), true);
 
     await driver.quit();
   });
