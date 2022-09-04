4b3f0f356622b54f8b3fa3b9e219b83e922195d0
mikeg Gronka <michal.gronka@tomandco.co.uk>
2022-09-04 19:21:27 +0200

    Wait until element is visible added to both scenarios

--- test/scenario1.spec.js
+++ test/scenario1.spec.js
@@ -1,18 +1,16 @@
-const { Builder, By } = require("selenium-webdriver");
+const { Builder, By, until } = require("selenium-webdriver");
 const assert = require("assert");
 
 describe("Test scenario 1", function () {
   it("Opens SmartFrame 1, hovers over the SmartFrame, checks if the caption is correctly displayed, clicks on the icon in the top-left-hand corner and expects the layer to open", async function () {
-    let driver = await new Builder().forBrowser("chrome").build();
+    const driver = await new Builder().forBrowser("chrome").build();
 
     await driver.get("https://smartitnow.blogspot.com/p/e.html");
 
     // Mouse over the SmartFrame
-    await driver.wait(
-      () => driver.findElement(By.className("float-container")).isDisplayed(),
-      20000
-    );
-    const hoverable = driver.findElement(By.className("float-container"));
+    const hoverable = await driver.findElement(By.className("float-container"));
+
+    await driver.wait(until.elementIsVisible(hoverable), 20000);
 
     const actions = driver.actions({
       async: true,
@@ -29,15 +27,9 @@ describe("Test scenario 1", function () {
       .frame(driver.findElement(By.className("smartframe-embed")));
 
     // Check if the caption is correctly displayed
-    await driver.wait(
-      () => driver.findElement(By.className("caption-wrapper")).getText(),
-      20000
-    );
-
-    const actualText = await driver
-      .findElement(By.className("caption-wrapper"))
-      .getText();
-
+    const caption = await driver.findElement(By.className("caption-wrapper"));
+    await driver.wait(until.elementIsVisible(caption), 20000);
+    const actualText = await caption.getText();
     const expectedText =
       "An image (from Latin: imago) is an artifact that depicts visual perception, such as a photograph or other two-dimensional picture, that resembles a subject—usually a physical object—and thus provides a depiction of it. In the context of signal processing, an image is a distributed amplitude of color(s). A pictorial script is a writing system that employs images as symbols for various semantic entities, rather than the abstract signs used by alphabets.";
     assert.strictEqual(actualText, expectedText);
@@ -47,7 +39,7 @@ describe("Test scenario 1", function () {
 
     // Check if the layer opens
     const layer = await driver.findElement(By.className("hide-copyright"));
-    await driver.wait(() => layer.isDisplayed(), 20000);
+    await driver.wait(until.elementIsVisible(layer), 20000);
     assert.strictEqual(await layer.isDisplayed(), true);
 
     await driver.quit();
