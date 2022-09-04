9132d71b6eca4db7972ccc02357fad6fd8b4142c
mikeg Gronka <michal.gronka@tomandco.co.uk>
2022-09-04 22:29:04 +0200

    It separation inside test scenarios

--- test/integration/scenario1.spec.js
+++ test/integration/scenario1.spec.js
@@ -4,47 +4,42 @@ const { Builder, until } = require("selenium-webdriver");
 const assert = require("assert");
 import { data } from "../fixtures/data.js";
 import { selectors } from "../fixtures/selectors.js";
+const driver = await new Builder().forBrowser("chrome").build();
 
 describe("Test scenario 1", function () {
-  it("Opens SmartFrame 1, hovers over the SmartFrame, checks if the caption is correctly displayed, clicks on the icon in the top-left-hand corner and expects the layer to open", async function () {
-    const driver = await new Builder().forBrowser("chrome").build();
-
+  it("Opens SmartFrame 1", async function () {
     await driver.get(data.path.smartFrame1);
+  });
 
-    // Mouse over the SmartFrame
+  it("Hovers over the SmartFrame", async function () {
     const hoverable = await driver.findElement(selectors.hoverable);
-
     await driver.wait(until.elementIsVisible(hoverable), 20000);
-
     const actions = driver.actions({
       async: true,
     });
-
     await actions
       .move({
         origin: hoverable,
       })
       .perform();
+  });
 
+  it("Checks if the caption is displayed correctly", async function () {
     await driver.switchTo().frame(driver.findElement(selectors.frame));
-
-    // Check if the caption is correctly displayed
     const caption = await driver.findElement(selectors.caption);
-
     await driver.wait(until.elementIsVisible(caption), 20000);
-
     const actualText = await caption.getText();
     assert.strictEqual(actualText, data.expectedText.caption);
+  });
 
-    // Click on the icon in the top-left-hand corner of the SmartFrame
+  it("Clicks on the icon in the top-left-hand corner and expects the layer to open", async function () {
     await driver.findElement(selectors.embedButton).click();
-
-    // Check if the layer opens
     const layer = await driver.findElement(selectors.layer);
-
     await driver.wait(until.elementIsVisible(layer), 20000);
     assert.strictEqual(await layer.isDisplayed(), true);
+  });
 
+  it("Closes browser", async function () {
     await driver.quit();
   });
 });
--- test/integration/scenario2.spec.js
+++ test/integration/scenario2.spec.js
@@ -4,47 +4,42 @@ const { Builder, until } = require("selenium-webdriver");
 const assert = require("assert");
 import { data } from "../fixtures/data.js";
 import { selectors } from "../fixtures/selectors.js";
+const driver = await new Builder().forBrowser("chrome").build();
 
 describe("Test scenario 2", function () {
-  it("Opens SmartFrame 2, hovers over the SmartFrame, checks if the share button is displayed correctly, clicks on the icon in the top-left-hand corner and expects to get redirected", async function () {
-    const driver = await new Builder().forBrowser("chrome").build();
-
+  it("Opens SmartFrame 2", async function () {
     await driver.get(data.path.smartFrame2);
+  });
 
-    // Mouse over the SmartFrame
+  it("Hovers over the SmartFrame", async function () {
     const hoverable = await driver.findElement(selectors.hoverable);
-
     await driver.wait(until.elementIsVisible(hoverable), 20000);
-
     const actions = driver.actions({
       async: true,
     });
-
     await actions
       .move({
         origin: hoverable,
       })
       .perform();
+  });
 
-    // Check if the Share button is displayed correctly
+  it("Checks if the share button is displayed correctly", async function () {
     const shareButton = await driver.findElement(selectors.shareButton);
-
     await driver.wait(until.elementIsVisible(shareButton), 20000);
-
     const actualText = await shareButton.getText();
     assert.strictEqual(actualText, data.expectedText.shareButton);
+  });
 
-    // Click on the icon in the top-left-hand corner of the SmartFrame
+  it("Clicks on the icon in the top-left-hand corner and expects to get redirected", async function () {
     await driver.findElement(selectors.seButton).click();
-
-    // Check if the redirect works
     const tabs = await driver.getAllWindowHandles();
-
     await driver.switchTo().window(tabs[1]);
-
     const actualUrl = await driver.getCurrentUrl();
     assert.strictEqual(actualUrl, data.expectedText.smartFrameUrl);
+  });
 
+  it("Closes browser", async function () {
     await driver.quit();
   });
 });
